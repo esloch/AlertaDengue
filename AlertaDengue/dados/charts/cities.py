a87e210545c43a6f42d944f4db5191d300146c91
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2020-03-16 22:46:02 -0400

    Data refactoring initial (#9)

--- AlertaDengue/dados/charts/cities.py
+++ AlertaDengue/dados/charts/cities.py
@@ -1,7 +1,5 @@
-'''
-Module for plotting charts in the cities reports
-'''
-
+from copy import deepcopy
+from datetime import timedelta
 from time import mktime
 import json
 from django.utils.translation import gettext as _
@@ -381,9 +379,121 @@ class ReportCityCharts:
         return figure.to_html()
 
 
-'''
-Responsible for creating the charts to alerts page
-'''
+class ReportStateCharts:
+    """Charts used by Report State."""
+
+    @classmethod
+    def create_tweet_chart(
+        cls, df: pd.DataFrame, year_week: int, disease: str
+    ) -> str:
+        """
+        Create chart with tweet information.
+
+        Parameters
+        ----------
+        df : pd.DataFrame
+            Dataframe with tweet information
+        year_week : int
+            Year and week desired filter e.g.: 202002
+        disease : str, {'dengue', 'chik', 'zika'}
+            Disease name
+
+        Returns
+        -------
+        str
+            HTML with Plotly chart.
+        """
+        df = deepcopy(df)
+        ks_cases = ['casos notif. {}'.format(disease)]
+
+        # TODO: check this code
+        df.index.name = 'SE'
+        df_tweet = df.reset_index()[['SE', 'tweets'] + ks_cases]
+        df_tweet = df_tweet[df_tweet.SE >= year_week - 200]
+
+        df_tweet.rename(columns={'tweets': 'menções'}, inplace=True)
+
+        df_grp = (
+            df_tweet.groupby(df.index)[['menções'] + ks_cases]
+            .sum()
+            .reset_index()
+        )
+
+        df_grp['SE'] = df_grp.SE.map(
+            lambda v: '%s/%s' % (str(v)[:4], str(v)[-2:])
+        )
+
+        figure = make_subplots(specs=[[{"secondary_y": True}]])
+
+        figure.add_trace(
+            go.Scatter(
+                x=['SE'],
+                y=['menções'],
+                name='Menciones',
+                marker={'color': 'rgb(51, 172, 255)'},
+                text=df_grp.SE.map(lambda v: '{}'.format(str(v)[-2:])),
+                hoverinfo='text',
+                hovertemplate="Semana %{text} : %{y:.1f} Casos",
+            ),
+            secondary_y=True,
+        )
+
+        figure.add_trace(
+            go.Scatter(
+                x=['SE'],
+                y=ks_cases,
+                name='Casos',
+                marker={'color': 'rgb(255,150,0)'},
+                text=df_grp.SE.map(lambda v: '{}'.format(str(v)[-2:])),
+                hoverinfo='text',
+                hovertemplate="Semana %{text} : %{y:.1f} Casos",
+            ),
+            secondary_y=False,
+        )
+
+        figure.update_layout(
+            title='Menções mídia social',
+            xaxis=dict(
+                title='Período (Ano/Semana)',
+                tickangle=-60,
+                nticks=len(ks_cases) // 4,
+                showline=True,
+                showgrid=True,
+                showticklabels=True,
+                linecolor='rgb(204, 204, 204)',
+                linewidth=0,
+                gridcolor='rgb(176, 196, 222)',
+            ),
+            yaxis=dict(
+                title='Temperatura',
+                showline=True,
+                showgrid=True,
+                showticklabels=True,
+                linecolor='rgb(204, 204, 204)',
+                linewidth=0,
+                gridcolor='rgb(176, 196, 222)',
+            ),
+            showlegend=True,
+            plot_bgcolor='rgb(255, 255, 255)',
+            paper_bgcolor='rgb(245, 246, 249)',
+            width=1100,
+            height=500,
+        )
+
+        figure.update_yaxes(title_text="Y_axies", secondary_y=True)
+
+        figure['layout']['legend'].update(
+            x=-0.1,
+            y=1.2,
+            traceorder='normal',
+            font=dict(family='sans-serif', size=12, color='#000'),
+            bgcolor='#FFFFFF',
+            bordercolor='#E2E2E2',
+            borderwidth=1,
+        )
+
+        return figure.to_html()
+
 
 
 class CityCharts:
